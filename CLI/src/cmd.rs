06a6fc2e79d589cf74c3071b5a50b88ada23e8a7
Liangcheng Juves <email@lcjuves.com>
2023-08-10 20:36:58 +0800

    Update code for setup network

--- CLI/src/cmd.rs
+++ CLI/src/cmd.rs
@@ -6,92 +6,54 @@
 //! </br>
 //!
 //! ```sh
-//! alias proxy='export http_proxy="${http_protocol_proxy_url}" && \
-//! export https_proxy="${http_protocol_proxy_url}" && \
-//! export all_proxy="socks5://$proxy_host_ip_addr:$proxy_host_port" && \
-//! setup_proxy && \
+//! alias proxy='export all_proxy="socks5://$proxy_host_ip_addr:$proxy_host_port" && \
 //! networksetup -setwebproxystate $networkservice on && \
 //! networksetup -setsecurewebproxystate $networkservice on && \
-//! networksetup -setsocksfirewallproxystate $networkservice on && \
-//! sudo dscacheutil -flushcache && \
-//! sudo killall -HUP mDNSResponder'
+//! networksetup -setsocksfirewallproxystate $networkservice on'
 //! ```
 
+#[cfg(target_os = "macos")]
+use std::ffi::OsStr;
 use std::io::Result;
-use std::process::{Command, Stdio};
+#[cfg(target_os = "macos")]
+use std::process::{Command, ExitStatus, Stdio};
 
-pub(crate) const SOCKS5_PROXY_HOST_IP_ADDR: &'static str = "127.0.0.1";
-pub(crate) const SOCKS5_PROXY_HOST_PORT: &'static str = "19934";
+pub(crate) const SOCKS5_PROXY_HOST_PORT: u16 = 19934;
 
 #[cfg(target_os = "macos")]
 pub(crate) const NETWORK_SERVICE: &'static str = "Wi-Fi";
 
 #[cfg(target_os = "macos")]
+#[inline]
+fn exec_networksetup<S: AsRef<OsStr>>(args: &[S]) -> Result<ExitStatus> {
+    let mut cmd_networksetup = Command::new("networksetup");
+    let mut cmd_networksetup = cmd_networksetup.stdout(Stdio::null()).stderr(Stdio::null());
+    for arg in args {
+        cmd_networksetup = cmd_networksetup.arg(arg);
+    }
+    cmd_networksetup.status()
+}
+
+#[cfg(target_os = "macos")]
 #[allow(unused_variables)]
-pub(crate) fn open_socks5_proxy(usr: &str, pwd: &str) -> Result<()> {
-    assert!((Command::new("networksetup")
-        .arg("-setsocksfirewallproxy")
-        .arg(NETWORK_SERVICE)
-        .arg(SOCKS5_PROXY_HOST_IP_ADDR)
-        .arg(SOCKS5_PROXY_HOST_PORT)
-        // .arg("off")
-        // .arg(usr)
-        // .arg(pwd)
-        .stdout(Stdio::null())
-        .stderr(Stdio::null())
-        .status()?)
+#[allow(dead_code)]
+pub(crate) fn open_socks5_proxy(ip: &str, usr: &str, pwd: &str) -> Result<()> {
+    assert!(exec_networksetup(&[
+        "-setsocksfirewallproxy",
+        NETWORK_SERVICE,
+        ip,
+        &SOCKS5_PROXY_HOST_PORT.to_string()
+    ])?
     .success());
 
-    assert!((Command::new("networksetup")
-        .arg("-setsocksfirewallproxystate")
-        .arg(NETWORK_SERVICE)
-        .arg("on")
-        .stdout(Stdio::null())
-        .stderr(Stdio::null())
-        .status()?)
-    .success());
-
-    assert!((Command::new("networksetup")
-        .arg("-setwebproxystate")
-        .arg(NETWORK_SERVICE)
-        .arg("off")
-        .stdout(Stdio::null())
-        .stderr(Stdio::null())
-        .status()?)
-    .success());
-
-    assert!((Command::new("networksetup")
-        .arg("-setsecurewebproxystate")
-        .arg(NETWORK_SERVICE)
-        .arg("off")
-        .stdout(Stdio::null())
-        .stderr(Stdio::null())
-        .status()?)
-    .success());
-
-    #[cfg(not(debug_assertions))]
-    assert!((Command::new("sudo").arg("dscacheutil").arg("-flushcache").status()?).success());
-    #[cfg(not(debug_assertions))]
-    assert!(
-        (Command::new("sudo").arg("killall").arg("-HUP").arg("mDNSResponder").status()?).success()
-    );
+    assert!(exec_networksetup(&["-setwebproxystate", NETWORK_SERVICE, "off"])?.success());
+    assert!(exec_networksetup(&["-setsecurewebproxystate", NETWORK_SERVICE, "off"])?.success());
+    assert!(exec_networksetup(&["-setsocksfirewallproxystate", NETWORK_SERVICE, "on"])?.success());
     Ok(())
 }
 
 #[cfg(target_os = "macos")]
 pub(crate) fn close_socks5_proxy() -> Result<()> {
-    assert!((Command::new("networksetup")
-        .arg("-setsocksfirewallproxystate")
-        .arg(NETWORK_SERVICE)
-        .arg("off")
-        .status()?)
-    .success());
-
-    #[cfg(not(debug_assertions))]
-    assert!((Command::new("sudo").arg("dscacheutil").arg("-flushcache").status()?).success());
-    #[cfg(not(debug_assertions))]
-    assert!(
-        (Command::new("sudo").arg("killall").arg("-HUP").arg("mDNSResponder").status()?).success()
-    );
+    assert!(exec_networksetup(&["-setsocksfirewallproxystate", NETWORK_SERVICE, "off"])?.success());
     Ok(())
 }
